2a1a44e71ca095794becef16fc0db6b1af35a4bb
Matti Nilsson <matti.nilsson980105@gmail.com>
2019-11-15 18:39:32 +0100

    Added a dumb way to make win conditions, and styling

--- Tic.js
+++ Tic.js
@@ -6,11 +6,13 @@ let gameBorder = document.getElementById("gameBorder");
 let whichPlayer = "playerOne";
 console.log(boards);
 let takenBoards = [];
-
+let winnerCheck = [];
+let winner = "";
+let moves = 0;
 
 for(let i = 0; i < boards.length; i++){
     boards[i].addEventListener("mouseup", () =>{
-        if(boards[i].id !== takenBoards[i]){
+        if(boards[i].id !== takenBoards[i] && winner === ""){
             newMark = document.createElement("img");
             if(whichPlayer === "playerOne"){
                 newMark.src = playerOneMark.src;
@@ -19,8 +21,12 @@ for(let i = 0; i < boards.length; i++){
             }
             boards[i].appendChild(newMark);
             takenBoards[i] = boards[i].id;
+            winnerCheck[i] = whichPlayer;
+            moves++;
             whoToPlay();
-        }else if(boards[i].id === takenBoards[i]){
+            check();
+            console.log(winnerCheck)
+        }else if(boards[i].id === takenBoards[i] && winner === ""){
             alert("Already Taken");
         }
     });
@@ -29,13 +35,125 @@ for(let i = 0; i < boards.length; i++){
 function whoToPlay(){
     if(whichPlayer === "playerOne"){
         showWhichPlayer.textContent = "Player Two's turn"
-        showWhichPlayer.setAttribute("style", "color : red");
         gameBorder.setAttribute("style", "border: 10px solid red")
         return whichPlayer = "playerTwo";
     }else if(whichPlayer === "playerTwo"){
         showWhichPlayer.textContent = "Player One's turn"
-        showWhichPlayer.setAttribute("style", "color : black");
         gameBorder.setAttribute("style", "border: 10px solid black")
         return whichPlayer = "playerOne"
+    }
+}
+
+function check(){
+    if(winnerCheck[0] === "playerOne" &&
+    winnerCheck[1] === "playerOne" &&
+    winnerCheck[2] === "playerOne"){
+        console.log("p1 Wins")
+        winner = "playerOne";
+    }
+    if(winnerCheck[3] === "playerOne" &&
+    winnerCheck[4] === "playerOne" &&
+    winnerCheck[5] === "playerOne"){
+        console.log("p1 Wins")
+        winner = "playerOne";
+    }
+    if(winnerCheck[6] === "playerOne" &&
+    winnerCheck[7] === "playerOne" &&
+    winnerCheck[8] === "playerOne"){
+        console.log("p1 Wins")
+        winner = "playerOne";
+    }
+    if(winnerCheck[0] === "playerOne" &&
+    winnerCheck[3] === "playerOne" &&
+    winnerCheck[6] === "playerOne"){
+        console.log("p1 Wins")
+        winner = "playerOne";
+    }
+    if(winnerCheck[1] === "playerOne" &&
+    winnerCheck[4] === "playerOne" &&
+    winnerCheck[7] === "playerOne"){
+        console.log("p1 Wins")
+        winner = "playerOne";
+    }
+    if(winnerCheck[2] === "playerOne" &&
+    winnerCheck[5] === "playerOne" &&
+    winnerCheck[8] === "playerOne"){
+        console.log("p1 Wins")
+        winner = "playerOne";
+    }
+    if(winnerCheck[0] === "playerOne" &&
+    winnerCheck[4] === "playerOne" &&
+    winnerCheck[8] === "playerOne"){
+        console.log("p1 Wins")
+        winner = "playerOne";
+    }
+    if(winnerCheck[2] === "playerOne" &&
+    winnerCheck[4] === "playerOne" &&
+    winnerCheck[6] === "playerOne"){
+        console.log("p1 Wins")
+        winner = "playerOne";
+    }
+
+    if(winnerCheck[0] === "playerTwo" &&
+    winnerCheck[1] === "playerTwo" &&
+    winnerCheck[2] === "playerTwo"){
+        console.log("p2 Wins")
+        winner = "playerTwo";
+    }
+    if(winnerCheck[3] === "playerTwo" &&
+    winnerCheck[4] === "playerTwo" &&
+    winnerCheck[5] === "playerTwo"){
+        console.log("p2 Wins")
+        winner = "playerTwo";
+    }
+    if(winnerCheck[6] === "playerTwo" &&
+    winnerCheck[7] === "playerTwo" &&
+    winnerCheck[8] === "playerTwo"){
+        console.log("p2 Wins")
+        winner = "playerTwo";
+    }
+    if(winnerCheck[0] === "playerTwo" &&
+    winnerCheck[3] === "playerTwo" &&
+    winnerCheck[6] === "playerTwo"){
+        console.log("p2 Wins")
+        winner = "playerTwo";
+    }
+    if(winnerCheck[1] === "playerTwo" &&
+    winnerCheck[4] === "playerTwo" &&
+    winnerCheck[7] === "playerTwo"){
+        console.log("p2 Wins")
+        winner = "playerTwo";
+    }
+    if(winnerCheck[2] === "playerTwo" &&
+    winnerCheck[5] === "playerTwo" &&
+    winnerCheck[8] === "playerTwo"){
+        console.log("p2 Wins")
+        winner = "playerTwo";
+    }
+    if(winnerCheck[0] === "playerTwo" &&
+    winnerCheck[4] === "playerTwo" &&
+    winnerCheck[8] === "playerTwo"){
+        console.log("p2 Wins")
+        winner = "playerTwo";
+    }
+    if(winnerCheck[2] === "playerTwo" &&
+    winnerCheck[4] === "playerTwo" &&
+    winnerCheck[6] === "playerTwo"){
+        console.log("p2 Wins")
+        winner = "playerTwo";
+    }
+
+    if(winner === "" && moves === 9){
+        showWhichPlayer.textContent = "IT'S A TIE";
+        showWhichPlayer.setAttribute("style", "color : White");
+        gameBorder.setAttribute("style", "border: 20px solid White")
+    }
+
+    if(winner === "playerOne"){
+        showWhichPlayer.textContent = "Player One Wins";
+        gameBorder.setAttribute("style", "border: 20px solid black")
+    }else if(winner === "playerTwo"){
+        showWhichPlayer.textContent = "Player Two Wins";
+        gameBorder.setAttribute("style", "border: 20px solid red")
     }
 }
